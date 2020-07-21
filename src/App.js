87ef649525d6a8023fc01335907a58c95965f772
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-21 17:31:37 -0300

    props e components II

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react' //1
 
-const Bemvindo = (props) => {
+const Equipe = (props) => {
     return (
-    <>
-        <h2>Bem-vindo(a), {props.nome}!</h2>
-        <h3>Tenho {props.idade} anos</h3>
-    </>
+        <div>
+            <Sobre username={props.nome} job={props.cargo} age={props.idade} />
+            <Social fb={props.facebook}  lin={props.linkedin} git={props.github} />
+        </div>
     )
 }
+
+const Sobre = props => {
+    return (
+        <>
+            <h2>Olá! Sou o(a) {props.username}. Tenho {props.age} e trabalho como {props.job}</h2>
+        </>
+    )
+}
+
+const Social = props => {
+    return (
+        <div>
+            <a href={props.fb}>Facebook</a>
+            <a href={props.lin}>LinkedIn</a>
+            <a href={props.git}>GitHub</a>
+            <hr />
+        </div>
+    )
+}
+
+
+
 
 //2
 function App() {
     return (
        <div>
-            <h1>Olá mundo</h1>
-            <Bemvindo nome="Matheus" idade="24" />
-            <Bemvindo nome="Amanda" idade="21" />
-            <h1>Curso de React</h1>
+            <h1>Conheça nossa equipe</h1>
+            <Equipe nome="Lucas" cargo="Programador" idade="29" />
+            <Equipe nome="Lucia Mara" cargo="CTO" idade="35" facebook="https://www.facebook.com" linkedin="https://www.linkedin.com" github="https://www.github.com" />
        </div>
     )
 }
