a6eeb71e37ae8bf3852cab2ab1e458afc090b867
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-21 18:25:58 -0300

    aplicando class component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import React from 'react' //1
+import React, { Component } from 'react' //1
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre username={props.nome} job={props.cargo} age={props.idade} />
-            <Social fb={props.facebook}  lin={props.linkedin} git={props.github} />
-        </div>
-    )
+//4
+class Equipe extends Component {
+    render(){
+        return (
+            <>
+                <h2>Olá! Sou a(o) {this.props.nome}.</h2>
+                <h3>Cargo: {this.props.cargo} de uma empresa</h3>
+                <h3>Idade: {this.props.idade}</h3>
+            </>
+        )
+    }
 }
-
-const Sobre = props => {
-    return (
-        <>
-            <h2>Olá! Sou o(a) {props.username}. Tenho {props.age} e trabalho como {props.job}</h2>
-        </>
-    )
-}
-
-const Social = props => {
-    return (
-        <div>
-            <a href={props.fb}>Facebook</a>
-            <a href={props.lin}>LinkedIn</a>
-            <a href={props.git}>GitHub</a>
-            <hr />
-        </div>
-    )
-}
-
-
 
 
 //2
@@ -36,8 +19,7 @@ function App() {
     return (
        <div>
             <h1>Conheça nossa equipe</h1>
-            <Equipe nome="Lucas" cargo="Programador" idade="29" />
-            <Equipe nome="Lucia Mara" cargo="CTO" idade="35" facebook="https://www.facebook.com" linkedin="https://www.linkedin.com" github="https://www.github.com" />
+            <Equipe nome="Eliana" cargo="CTO" idade="34" />
        </div>
     )
 }
