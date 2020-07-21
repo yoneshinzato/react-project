8ed7aba7fd136c8a21f19da33676483745f393c9
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-21 16:15:43 -0300

    App

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-project
+1-npx create-react-app .
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react' //1
+
+//2
+function App() {
+    return (
+        <h1>Olá mundo</h1>
+    )
+}
+
+export default App //3
