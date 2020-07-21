03a27acd68d5ff9f248f9c24d4062acb1c018386
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-21 17:08:21 -0300

    props e component I

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react' //1
 
+const Bemvindo = (props) => {
+    return (
+    <>
+        <h2>Bem-vindo(a), {props.nome}!</h2>
+        <h3>Tenho {props.idade} anos</h3>
+    </>
+    )
+}
+
 //2
 function App() {
     return (
-        <h1>Olá mundo</h1>
+       <div>
+            <h1>Olá mundo</h1>
+            <Bemvindo nome="Matheus" idade="24" />
+            <Bemvindo nome="Amanda" idade="21" />
+            <h1>Curso de React</h1>
+       </div>
     )
 }
 
