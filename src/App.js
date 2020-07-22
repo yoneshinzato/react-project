ba080359cd91b8c51eb2394fdca529b36f88f2d5
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-22 17:51:23 -0300

    events 1

--- src/App.js
+++ src/App.js
@@ -4,32 +4,11 @@ import React, { Component } from 'react'
 
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      hora: "00:00:00"
-    }
-  }
-
-  componentDidMount(){
-    setInterval(()=>{
-      this.setState({hora: new Date().toLocaleTimeString()})
-    }, 1000)
-  }
-
-  componentDidUpdate(){
-    console.log('Atualizou!')
-  }
-
-  // shouldComponentUpdate(){
-
-  // }
-
   render(){
     return (
-      <div>
-        <h1>My project {this.state.hora}</h1>
-      </div>
+      <>
+        <h1>Hellow</h1>
+      </>
     )
   }
 }
