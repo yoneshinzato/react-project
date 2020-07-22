720ae44267eefe12136b8bdba655671cc9a72a99
Yone Shinzato <yshinzato@fundasp.org.br>
2020-07-22 17:12:51 -0300

    ciclo de vida component componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+
+
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
     this.state = {
-      nome: "Yone",
-      contador: 0,
-    };
-    //só usa se for com function, com a arrow function funciona sem
-    // this.aumentar = this.aumentar.bind(this);
-    // this.diminuir = this.diminuir.bind(this);
+      hora: "00:00:00"
+    }
   }
 
-  aumentar = () => {
-    let batata = this.state;
-    batata.contador += 1;
-    batata.nome = "Ferdinando";
-    this.setState(batata);
-  };
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000)
+  }
 
-  diminuir = () => {
-    let tomate = this.state;
-    if (tomate.contador <= 0) {
-      alert("Ops, está abaixo de zero!");
-      return; //para parar
-    }
-    tomate.contador -= 1;
-    tomate.nome = "Afrânio";
-    this.setState(tomate);
-  };
-
-  render() {
+  render(){
     return (
-      <>
-        <h2>{this.state.nome}</h2>
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
-      </>
-    );
+      <div>
+        <h1>My project {this.state.hora}</h1>
+      </div>
+    )
   }
 }
 
-export default App;
+
+
+export default App
